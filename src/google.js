0d657f7c421ce7e4d5d2ec6b8a08082695f97cb1
AnaDamiao <damiaoclara2003@gmail.com>
2025-11-26 21:36:50 -0300

    Removendo log

--- src/google.js
+++ src/google.js
@@ -5,7 +5,4 @@ const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_SECRET,
     process.env.GOOGLE_REDIRECT_URI
 );
-
-console.log("CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("SECRET:", process.env.GOOGLE_CLIENT_SECRET);
 module.exports = oauth2Client;
